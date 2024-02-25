1c2cf3c716279a3f9e57233e2c723f134c90f993
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2024-02-25 15:08:32 -0500

    Add files via upload

---
+++ LAB01/lab1.md
@@ -0,0 +1,11 @@
+#
+# Author: Tarun Thomas
+# Student Number: 113605224
+#
+Python's speciality lies in its readability, simplicity, and a rich standard library that expedites development. The dynamic typing fosters flexibility, but the lack of strict type checking can lead to runtime errors. Unanticipated issues, such as case-sensitive string comparison, underline the importance of understanding Python's issues.
+
+Comparing Python with C/C++, both share features like functions and classes, yet differ in typing and memory management. Python's simplicity creates shorter, readable code, emphasizing high-level abstractions. In contrast, C/C++ demand explicit memory management and offer static typin. These distinctions shape my programming approach; Python encourages a focus on readability and flexibility, while C/C++ demand attention to low-level details and optimization.
+
+
+
+
--- LAB01/lab1.py
+++ LAB01/lab1.py
@@ -1,1 +1,61 @@
-print("Hello, World!")
+#
+# Author: Tarun Thomas
+# Student Number: 113605224
+#
+
+# Function 1
+def wins_rock_scissors_paper(player_throw, opponent_throw):
+    player_throw = player_throw.lower()
+    opponent_throw = opponent_throw.lower()
+
+    if (player_throw == 'rock' and opponent_throw == 'scissors') or \
+       (player_throw == 'paper' and opponent_throw == 'rock') or \
+       (player_throw == 'scissors' and opponent_throw == 'paper'):
+        return True
+    else:
+        return False
+
+# Function 2
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Function 3
+def Fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
+# Function 4
+def sum_to_goal(numbers, goal):
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == goal:
+                return numbers[i] * numbers[j]
+    return 0
+
+# Python class
+class UpCounter:
+    def __init__(self, step_size=1):
+        self.counter = 0
+        self.step_size = step_size
+
+    def count(self):
+        return self.counter
+
+    def update(self):
+        self.counter += self.step_size
+
+class DownCounter(UpCounter):
+    def update(self):
+        self.counter -= self.step_size
+
