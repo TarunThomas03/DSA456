b9561e7184f1eb329c9fce4db8fa6627d354f419
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2024-03-22 00:39:24 +0000

    Final Draft

--- ASSIGNMENT02/a2.py
+++ ASSIGNMENT02/a2.py
@@ -8,6 +8,7 @@ class SortedLinkedList:
         self.head = None
         self.size = 0
 
+
     def insert(self, data):
         new_node = Node(data)
         if self.head is None or data <= self.head.data:
